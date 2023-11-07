8cdb68b469f62cd6bdf4b808e550576c51495c46
Yu <awushu6@gmail.com>
2023-11-07 11:03:09 +0400

    day2

---
+++ week2/Day2/class/exercise-1/script.js
@@ -0,0 +1,13 @@
+debugger;
+let userInput = prompt("Enter age:");
+
+
+if (userInput < 18) {
+    alert("You are too young  to drive this car. Powering off")
+ } else if (userInput == 18) {
+    alert("congratualation on your first year of driving. Enjoy the ride. ")
+ } else {
+    if (userInput > 18) {
+        alert("powering on emjoy your ride")
+    }
+ }
---
+++ week2/Day2/class/exercise-2/script.js
@@ -0,0 +1,14 @@
+let userInput = prompt(" enter your age");
+
+switch (userInput) {
+    case  '18' :
+        alert("you are too young")
+        break;
+        case '18' :
+            alert("enjoy your first year")
+            break;
+         case '19' :
+            alert("powering on")
+            break;
+
+}
