394f75d3dc777c64d84f2c796518421d1cc099db
Yu <awushu6@gmail.com>
2023-11-13 15:18:37 +0400

    Refactor createFrame function to use let instead of const for variable declarations

--- week2/Day4/Daily-Challenge/script.js
+++ week2/Day4/Daily-Challenge/script.js
@@ -1,19 +1,19 @@
 function createFrame(words) {
     // Split the input string into an array of words
-    const wordArray = words.split(',').map(word => word.trim());
+    let wordArray = words.split(',').map(word => word.trim());
 
     // Find the length of the longest word
-    const maxLength = Math.max(...wordArray.map(word => word.length));
+    let maxLength = Math.max(...wordArray.map(word => word.length));
 
     // Create the top border of the frame
-    const topBorder = '*'.repeat(maxLength + 4);
+    let topBorder = '*'.repeat(maxLength + 4);
 
     // Initialize the frame string with the top border
     let frame = topBorder + '<br>';
 
     // Add the words with side borders to the frame string
     wordArray.forEach(word => {
-        const spaces = ' '.repeat(maxLength - word.length);
+        let spaces = ' '.repeat(maxLength - word.length);
         frame += `* ${word}${spaces} *<br>`;
     });
 
@@ -25,7 +25,7 @@ function createFrame(words) {
 }
 
 // Prompt the user for input
-const userInput = prompt("Enter several words separated by commas:");
+let userInput = prompt("Enter several words separated by commas:");
 
 // Display the words in a rectangular frame on the page
 document.write(createFrame(userInput));
