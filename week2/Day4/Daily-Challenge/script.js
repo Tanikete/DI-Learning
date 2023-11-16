0edd35f96a30661002c78e542437bddf4920b098
Yu <awushu6@gmail.com>
2023-11-16 09:54:20 +0400

    Refactor frame creation function to improve readability and console output

--- week2/Day4/Daily-Challenge/script.js
+++ week2/Day4/Daily-Challenge/script.js
@@ -9,18 +9,16 @@ function createFrame(words) {
     let topBorder = '*'.repeat(maxLength + 4);
 
     // Initialize the frame string with the top border
-    let frame = topBorder + '<br>';
+    let frame = topBorder + '\n';
 
     // Add the words with side borders and proper alignment to the frame string
     wordArray.forEach(word => {
         let spaces = ' '.repeat(maxLength - word.length);
-        frame += `* ${spaces}${word} *<br>`;
+        frame += `* ${word}${spaces} *\n`;
     });
 
     // Add the bottom border to the frame string
     frame += topBorder;
-   
-
 
     // Return the frame string
     return frame;
@@ -29,5 +27,6 @@ function createFrame(words) {
 // Prompt the user for input
 let userInput = prompt("Enter several words separated by commas:");
 
-// Display the words in a rectangular frame on the page
-document.write(createFrame(userInput));
+// Display the words in a rectangular frame on the console
+console.log(createFrame(userInput));
+
