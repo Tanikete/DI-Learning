56c726b79bba0f460bed831925cf098f1f738810
Yu <awushu6@gmail.com>
2023-11-23 11:48:40 +0400

    Add current timestamp to console output

--- week4/Day4/class/script.js
+++ week4/Day4/class/script.js
@@ -11,3 +11,5 @@ let blackRabbit = new Rabbit("black");
 killerRabbit.speak("i need food & you seem the only viable option")
 blackRabbit.speak('i am black & i am proud ')
 
+now = new Date();
+console.log(now.getTime()/1000);
