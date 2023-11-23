3583bfedcd8697ae3d0c7e693891928e9b72fd3f
Yu <awushu6@gmail.com>
2023-11-23 11:33:11 +0400

    Fix bug in login functionality

---
+++ week4/Day4/class/script.js
@@ -0,0 +1,13 @@
+class Rabbit {
+    constructor(type) {
+        this.type = type;
+    }
+    speak(line) {
+        console.log(`The ${this.type} rabbit says '${line}'`);
+    }
+}
+let killerRabbit = new Rabbit("killer");
+let blackRabbit = new Rabbit("black");
+killerRabbit.speak("i need food & you seem the only viable option")
+blackRabbit.speak('i am black & i am proud ')
+
