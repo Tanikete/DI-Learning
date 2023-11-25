2d216bdb67b1be68c655978d9928b26b298ee956
Yu <awushu6@gmail.com>
2023-11-25 08:26:33 +0400

    Remove unnecessary code and update page title

--- week4/Day5/Daily-Challenge/script.js
+++ week4/Day5/Daily-Challenge/script.js
@@ -1,124 +1,20 @@
-const robots = [
-    {
-      id: 1,
-      name: 'Leanne Graham',
-      username: 'Bret',
-      email: 'Sincere@april.biz',
-      image: 'https://robohash.org/1?200x200'
-    },
-    {
-      id: 2,
-      name: 'Ervin Howell',
-      username: 'Antonette',
-      email: 'Shanna@melissa.tv',
-      image: 'https://robohash.org/2?200x200'
-    },
-    {
-      id: 3,
-      name: 'Clementine Bauch',
-      username: 'Samantha',
-      email: 'Nathan@yesenia.net',
-      image: 'https://robohash.org/3?200x200'
-    },
-    {
-      id: 4,
-      name: 'Patricia Lebsack',
-      username: 'Karianne',
-      email: 'Julianne.OConner@kory.org',
-      image: 'https://robohash.org/4?200x200'
-    },
-    {
-      id: 5,
-      name: 'Chelsey Dietrich',
-      username: 'Kamren',
-      email: 'Lucio_Hettinger@annie.ca',
-      image: 'https://robohash.org/5?200x200'
-    },
-    {
-      id: 6,
-      name: 'Mrs. Dennis Schulist',
-      username: 'Leopoldo_Corkery',
-      email: 'Karley_Dach@jasper.info',
-      image: 'https://robohash.org/6?200x200'
-    },
-    {
-      id: 7,
-      name: 'Kurtis Weissnat',
-      username: 'Elwyn.Skiles',
-      email: 'Telly.Hoeger@billy.biz',
-      image: 'https://robohash.org/7?200x200'
-    },
-    {
-      id: 8,
-      name: 'Nicholas Runolfsdottir V',
-      username: 'Maxime_Nienow',
-      email: 'Sherwood@rosamond.me',
-      image: 'https://robohash.org/8?200x200'
-    },
-    {
-      id: 9,
-      name: 'Glenna Reichert',
-      username: 'Delphine',
-      email: 'Chaim_McDermott@dana.io',
-      image:'https://robohash.org/9?200x200'
-    },
-    {
-      id: 10,
-      name: 'Clementina DuBuque',
-      username: 'Moriah.Stanton',
-      email: 'Rey.Padberg@karina.biz',
-      image:'https://robohash.org/10?200x200'
-    }
-    ];
-    
+function isAnagram(str1, str2) {
+  // Remove whitespace and convert strings to lowercase
+  str1 = str1.replace(/\s/g, '').toLowerCase();
+  str2 = str2.replace(/\s/g, '').toLowerCase();
 
-    const Search = document.getElementById("search");
-    const container = document.getElementById("robots");
-//method#1
-    // const searchRobot = () => {
-    //     container.innerHTML = "";
-    //     const search = Search.value;
-    //     robots.forEach(robot => {
-    //         if (robot.name.toLowerCase().includes(search.toLowerCase())) {
-    //             const card = document.createElement("div");
-    //             card.classList.add("card");
-    //             card.innerHTML = `
-    //                 <img src="${robot.image}" alt="robot">
-    //                 <h3>${robot.name}</h3>
-    //                 <p>${robot.email}</p>
-    //             `;
-    //             container.appendChild(card);
-    //         }
-    //     });
-    // }
+  // Check if the lengths of the strings are equal
+  if (str1.length !== str2.length) {
+    return false;
+  }
 
-    // Search.addEventListener("keyup", searchRobot);
-    // searchRobot()
+  // Sort the characters in both strings
+  const sortedStr1 = str1.split('').sort().join('');
+  const sortedStr2 = str2.split('').sort().join('');
 
-    //method#2
-// container.innerHTML = robots.map(robot => {
-//     return `
-//         <div class="card">
-//             <img src="${robot.image}" alt="robot">
-//             <h3>${robot.name}</h3>
-//             <p>${robot.email}</p>
-//         </div>
-//     `;
-// }).join("");
-// Search.addEventListener("keyup", () => {
-//     const search = Search.value;
-//     container.innerHTML = robots.filter(robot => robot.name.toLowerCase().includes(search.toLowerCase())).map(robot => {
-//         return `
-//             <div class="card">
-//                 <img src="${robot.image}" alt="robot">
-//                 <h3>${robot.name}</h3>
-//                 <p>${robot.email}</p>
-//             </div>
-//         `;
-//     }).join("");
-// });
+  // Compare the sorted strings
+  return sortedStr1 === sortedStr2;
+}
 
-
-
-
-
+const check = isAnagram('silent', 'listen');
+console.log(check);
