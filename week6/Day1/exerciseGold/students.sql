ccef92dfec4475b7af49a3602b052b57ce5afca2
Yu <awushu6@gmail.com>
2023-12-05 13:53:27 +0400

    Fix bug in login functionality

---
+++ week6/Day1/exerciseGold/students.sql
@@ -0,0 +1,7 @@
+-- SELECT * FROM students
+
+-- SELECT first_name, last_name, birth_date FROM students ORDER BY last_name ASC LIMIT 4
+
+-- SELECT * FROM students ORDER BY birth_date DESC LIMIT 1
+
+-- SELECT * FROM students OFFSET 2
