e242a6aa651c19776201a1f0e9039025b4303c60
Yu <awushu6@gmail.com>
2023-12-10 17:33:58 +0400

    Refactor database schema and functions

--- week6/Day4/daily-Challenge/users.sql
+++ week6/Day4/daily-Challenge/users.sql
@@ -1,51 +1,56 @@
--- CREATE TABLE users (
---     user_id SERIAL PRIMARY KEY,
---     username VARCHAR UNIQUE NOT NULL
--- );
-
--- CREATE TABLE product_orders (
---     order_id SERIAL PRIMARY KEY,
---     user_id INTEGER REFERENCES users(user_id) ON DELETE CASCADE,
---     order_date DATE NOT NULL
--- );
 
 -- CREATE TABLE items (
 --     item_id SERIAL PRIMARY KEY,
---     order_id INTEGER REFERENCES product_orders(order_id) ON DELETE CASCADE,
---     product_name VARCHAR NOT NULL,
---     quantity INTEGER NOT NULL,
---     price DECIMAL(10, 2) NOT NULL
+--     product_name VARCHAR(255),
+--     price DECIMAL(10, 2)
 -- );
 
--- CREATE OR REPLACE FUNCTION calculate_order_total(order_id_input INTEGER)
--- RETURNS DECIMAL(10, 2) AS $$
--- DECLARE
---     total DECIMAL(10, 2);
--- BEGIN
---     SELECT SUM(price * quantity)
---     INTO total
---     FROM items
---     WHERE order_id = order_id_input;
 
---     RETURN COALESCE(total, 0);
+-- CREATE TABLE product_orders (
+--     order_id SERIAL PRIMARY KEY,
+--     order_date DATE
+-- );
+
+-- CREATE TABLE order_items (
+--     order_id INT REFERENCES product_orders(order_id),
+--     item_id INT REFERENCES items(item_id),
+--     quantity INT,
+--     PRIMARY KEY (order_id, item_id)
+-- );
+
+-- INSERT INTO items (product_name, price) VALUES
+--     ('Product A', 10.00),
+--     ('Product B', 15.00),
+--     ('Product C', 20.00);
+
+
+-- INSERT INTO product_orders (order_date) VALUES
+--     ('2023-01-15'),
+--     ('2023-02-20');
+
+
+-- INSERT INTO order_items (order_id, item_id, quantity) VALUES
+--     (1, 1, 2),
+--     (1, 2, 1),
+--     (2, 3, 3),
+--     (2, 1, 1);
+
+
+-- CREATE OR REPLACE FUNCTION calculate_total_price(p_order_id INT)
+-- RETURNS DECIMAL AS $$
+-- DECLARE
+--     total_price DECIMAL := 0;
+-- BEGIN
+--     SELECT SUM(items.price * order_items.quantity)
+--     INTO total_price
+--     FROM order_items
+--     JOIN items ON order_items.item_id = items.item_id
+--     WHERE order_items.order_id = p_order_id;
+
+--     RETURN total_price;
 -- END;
 -- $$ LANGUAGE plpgsql;
 
--- CREATE OR REPLACE FUNCTION calculate_user_order_total(user_id_input INTEGER, order_id_input INTEGER)
--- RETURNS DECIMAL(10, 2) AS $$
--- DECLARE
---     total DECIMAL(10, 2);
--- BEGIN
---     SELECT SUM(i.price * i.quantity)
---     INTO total
---     FROM items i
---     JOIN product_orders po ON i.order_id = po.order_id
---     WHERE i.order_id = order_id_input AND po.user_id = user_id_input;
 
---     RETURN COALESCE(total, 0);
--- END;
--- $$ LANGUAGE plpgsql;
-
-
-
+SELECT calculate_total_price(1) AS total_price;
 
