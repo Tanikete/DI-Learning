3aa400decc2dbf5e646fd9f633e332bd5c8e967e
Yu <awushu6@gmail.com>
2024-01-18 11:00:04 +0400

    Refactor App.js to import and render new components

--- week8/Day2/exerciseXP/exercise1/src/App.js
+++ week8/Day2/exerciseXP/exercise1/src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Car from './Components/Car';
+import Garage from './Components/Garage';
+import Events from './Components/Events';
+import Phone from './Components/Phone';
+import Color from './Components/Color';
+
+const carInfo = { name: 'Ford', model: 'Mustang' };
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Car carInfo={carInfo} />
+      <Events />
+      <Phone />
+      <Color />
     </div>
   );
 }
