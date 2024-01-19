55628944ed91fc3b9a9e83bfa96e6b89b2bb3a96
Yu <awushu6@gmail.com>
2024-01-19 23:31:19 +0400

    Add SQLite3 dependency and update App component

--- week8/Day5/exerciseXP/todo/src/App.js
+++ week8/Day5/exerciseXP/todo/src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [text, setText] = useState('');
+  const [todos, setTodos] = useState([]);
+
+  const handleInputChange = (event) => {
+    setText(event.target.value);
+  };
+
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
+    setTodos([...todos, { text, crossed: false }]);
+    setText('');
+  };
+
+  const handleTodoClick = (index) => {
+    const newTodos = [...todos];
+    newTodos[index].crossed = !newTodos[index].crossed;
+    setTodos(newTodos);
+  };
+
+  const handleTodoDoubleClick = (index) => {
+    const newTodos = todos.filter((_, todoIndex) => todoIndex !== index);
+    setTodos(newTodos);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={handleFormSubmit}>
+        <input type="text" value={text} onChange={handleInputChange} />
+        <button type="submit">Add Todo</button>
+      </form>
+      <ul>
+        {todos.map((todo, index) => (
+          <li
+            key={index}
+            onClick={() => handleTodoClick(index)}
+            onDoubleClick={() => handleTodoDoubleClick(index)}
+            className={todo.crossed ? 'completed' : ''}
+          >
+            {todo.text}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
